8e735c192ee341dce5ab690f4feeb0631c187d4b
J. David Beutel <git@getsu.com>
2014-06-14 13:05:44 -1000

    try adding explicit includes to the people URL mapping.
    I want to push this to Heroku to see if it enables a RESTful JSON get of people.  The explicit includes is the main difference that I see now between settings/password (which work on Heroku), and people.  I'm only guessing, though, because I can't reproduce the problem locally.

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -10,7 +10,7 @@ class UrlMappings {
         // REST
         "/settings"(resource:'settings', includes:['show', 'edit', 'update'])   // one per user, with implicit id from authentication
         "/password"(resource:'password', includes:['update'])                   // one per user, with implicit id from authentication
-        "/people"(resources:'person')
+        "/people"(resources:'person', includes:['index', 'show', 'save', 'update'])
 
         "/"(view:"/index")
         "500"(view:'/error')
