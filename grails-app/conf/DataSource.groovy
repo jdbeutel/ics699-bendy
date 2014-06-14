065cc4fe7f66e4c2c8773ea5ae09a86db7dce4c6
J. David Beutel <git@getsu.com>
2014-06-14 13:02:01 -1000

    changed prod db to "create-drop".
    I needed to do this to run the war in webapp-runner more than once locally.  Since the prod db is configured as a file, not mem, but the fixtures still try to load on bootstrap (because I need them to test on Heroku), the boot fails with duplicate errors.  So, for now, I'll just drop the prod db.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -28,7 +28,7 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
+            dbCreate = "create-drop"    // todo: postgres for heroku, and "update" w/out fixtures
             url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
             properties {
                maxActive = -1
