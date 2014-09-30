7af31e2f686160a9a5a8a28c912b74ced17582ee
J. David Beutel <git@getsu.com>
2014-09-29 16:15:27 -1000

    default elasticsearch config to not use external server

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -3,7 +3,7 @@ import com.getsu.wcy.User
 elasticSearch.datastoreImpl = 'hibernateDatastore'
 elasticSearch.bulkIndexOnStartup = false
 elasticSearch.disableAutoIndex = true
-elasticSearch.client.mode = 'node'    // for debugging w/ local elasticsearch server
+//elasticSearch.client.mode = 'node'    // uncomment to debug w/ (local) elasticsearch server
 
 // authentication plugin
 authenticationUserClass = User
